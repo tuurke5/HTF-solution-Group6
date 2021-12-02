39108c9a002220a604c48bed92bdf8862b41eb1d
arthur.de.groote <arthur.de.groote@student.howest.be>
2021-12-02 15:59:42 +0100

    een niet zo waardige commit

--- HTF/js.js
+++ HTF/js.js
@@ -1,35 +1,73 @@
-let caughtPokemon = []
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
   showSuspects();
 }
 
-function showSuspects(){
-    var requestOptions = {
+async function showSuspects(){
+      var myHeaders = new Headers();
+      myHeaders.append("userId", "matrixcoders991");
+  
+      var requestOptions = {
         method: 'GET',
+        headers: myHeaders,
         redirect: 'follow'
       };
+
+      var cars;
+      await fetch("https://htf-2021.zinderlabs.com/car", requestOptions)
+        .then(response => response.json())
+        .then(result => cars = result)
+        .catch(error => console.log('error', error));
+
+      var motives;
+      await fetch("https://htf-2021.zinderlabs.com/motive/", requestOptions)
+      .then(response => response.json())
+      .then(result => motives = result)
+      .catch(error => console.log('error', error));
+
+      var alibis;
+      await fetch("https://htf-2021.zinderlabs.com/alibi", requestOptions)
+      .then(response => response.json())
+      .then(result => alibis = result)
+      .catch(error => console.log('error', error));
+
       
-      fetch("https://htf-2021.zinderlabs.com/suspect", requestOptions)
+      
+      await fetch("https://htf-2021.zinderlabs.com/suspect", requestOptions)
         .then(response => response.json())
         .then(result => {
-            let res = ``;
+            let suspects = ``;
             result.forEach(el => {
-                res += `
-                <li id="${el.id}" class="sus"> ${el.name}
-                    <img src="${el.imgSrc}" alt="${el.name}" title="${el.name}"/>
-                </li>` 
+                let lp = cars.find(el2 => el2.owner === el.name)
+                if(lp == null){
+                  lp = "no car found"
+                }
+                let mtv = motives.find(el3 => el3.suspectId === el.id)
+                if(mtv == null){
+                  mtv= {text:"no motive"}
+                }
+                let alb = alibis.find(el4 => el4.suspectId === el.id)
+                if(alb == null){
+                  alb= {description:"geen alibi 🧐"}
+                }
+                suspects += `
+                <div class="col s2" id="${el.id}" style="height:60rem;">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="${el.imgSrc}" style="height:13rem; width:12.8rem">
+                    </div>
+                    <div class"card-content" style="height:37.2rem">
+                      <span class="card-title">${el.name}</span>
+                      <p>licenseplate: ${lp["licenseplate"]}<p>
+                      <p>motive: ${mtv["text"]}
+                      <p>alibi: ${alb["description"]}
+                    </div>    
+                  </div>
+                </div>
+                `
             });
-            document.querySelector("#suspects").innerHTML = res;
+            document.querySelector("#suspects").innerHTML = suspects;
         })
-        .then(document.querySelectorAll(".sus").forEach(item => {
-            item.addEventListener('click',showOneSuspect);  
-          }))
         .catch(error => console.log('error', error)); 
-}
-
-function showOneSuspect(e){
-    e.preventDefault();
-    console.log(e); 
 }
